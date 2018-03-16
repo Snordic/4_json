901599ca134ecd0110d50b7f0b3295c43b689c99
Snordic <artstr16@gmail.com>
2018-03-17 02:36:56 +0500

    Форматирование JSON
    Учебный проект

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return pretty_print_json(data)
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, ensure_ascii=False, sort_keys=True, indent=7)
 
 
 if __name__ == '__main__':
-    pass
+    filename = 'alco_shops.json'
+    print(load_data(filename))
+else:
+    print(load_data(sys.argv[1]))
