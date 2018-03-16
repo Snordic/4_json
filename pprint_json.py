76ac9811b1470ef5657405282e37d07bc23ef5a1
Snordic <artstr16@gmail.com>
2018-03-17 02:55:08 +0500

    Исправлены названия перем

--- pprint_json.py
+++ pprint_json.py
@@ -3,9 +3,9 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file_json:
-        data_json = json.load(file_json)
-    return pretty_print_json(data_json)
+    with open(filepath, 'r', encoding='utf-8') as file_with_json:
+        data = json.load(file_with_json)
+    return pretty_print_json(data)
 
 
 def pretty_print_json(data):
@@ -13,7 +13,7 @@ def pretty_print_json(data):
 
 
 if __name__ == '__main__':
-    filename = 'alco_shops.json'
-    print(load_data(filename))
+    file_name_json = 'alco_shops.json'
+    print(load_data(file_name_json))
 else:
     print(load_data(sys.argv[1]))
