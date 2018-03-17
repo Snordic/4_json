2c871f0087081c35b9d57c5b2d4acd05e4f554d1
Snordic <artstr16@gmail.com>
2018-03-17 17:56:57 +0500

    Исправлены название перемен

--- pprint_json.py
+++ pprint_json.py
@@ -4,16 +4,13 @@ import sys
 
 def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as file_with_json:
-        data_json = json.load(file_with_json)
-    return pretty_print_json(data_json)
+        data_from_file = json.load(file_with_json)
+    return data_from_file
 
 
-def pretty_print_json(json_handling):
-    return json.dumps(json_handling, ensure_ascii=False, sort_keys=True, indent=7)
+def pretty_print_json(data_output):
+    return print(json.dumps(data_output, ensure_ascii=False, sort_keys=True, indent=7))
 
 
 if __name__ == '__main__':
-    print(load_data(sys.argv[1]))
-else:
-    file_name_json = 'alco_shops.json'
-    print(load_data(file_name_json))
+    pretty_print_json(load_data(sys.argv[1]))
