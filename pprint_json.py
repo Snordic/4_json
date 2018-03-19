010f5fc4526459a05d3e85e1a39bc25786043ac0
Snordic <artstr16@gmail.com>
2018-03-19 20:00:46 +0500

    Исключения для работы с json

--- pprint_json.py
+++ pprint_json.py
@@ -8,9 +8,14 @@ def load_data(filepath):
     return data_from_file
 
 
-def pretty_print_json(data_output):
-    return print(json.dumps(data_output, ensure_ascii=False, sort_keys=True, indent=7))
+def pretty_print_json(data_from_json):
+    print(json.dumps(data_from_json, ensure_ascii=False, sort_keys=True, indent=7))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(sys.argv[1]))
+    try:
+        pretty_print_json(load_data(sys.argv[1]))
+    except IndexError: print('Ошибка: Вы не добавили файл!')
+    except TypeError: print('Ошибка: Вы не добавили файл!')
+    except json.decoder.JSONDecodeError: print('Ошибка: Расширение файла не JSON!')
+
